fdaa7841444f5a6655de9dfb1bd7b9abb02e1649
xiangge.lx <xiangge.lx@lxiange-MacBook.local>
2017-07-06 21:32:56 +0800

    init commit.

---
+++ README.md
@@ -0,0 +1,30 @@
+# 钉钉已读回执拦截器
+
+**适用于网页版钉钉**
+
+## 安装方法：
+
+1. 通过Chrome Webstore（此处有超链接）安装。
+2. 若无法访问Chrome Webstore，可下载此项目解压，然后打开`chrome://extensions/`，点击`加载已解压的扩展程序...`按钮，选择此项目文件夹。
+
+## 使用方法：
+
+安装插件后，登陆网页版钉钉正常使用即可。
+
+代码工作正常情况下，打开控制台，会打印
+```
+Injecting Hookish! hooks.
+Using wshook. All WebSocket connections are being hooked.
+```
+这两行信息，表示代码已经注入成功（此处有图）。
+![welcome]({{ site.image_url }}/posts/ding-interceptor/welcome.png)
+此外，每拦截一条已读回执，会在控制台打印一条信息：
+![log]({{ site.image_url }}/posts/ding-interceptor/log.png)
+（这堆error是钉钉代码的锅）
+
+此插件会拦截网页版钉钉发送的所有已读回执，如需发送已读回执，请在手机等其他客户端上再次浏览消息即可。
+
+
+## 致谢
+* [skepticfx/hookish](https://github.com/skepticfx/hookish)
+（其实基本上就是照着抄的）
---
+++ ding_interceptor.js
@@ -0,0 +1,76 @@
+var wshook = function () {
+    console.log('Using wsHook. All WebSocket connections are being hooked.');
+    /* wsHook.js
+     * https://github.com/skepticfx/wshook
+     * Reference: http://www.w3.org/TR/2011/WD-websockets-20110419/#websocket
+     */
+
+    var wsHook = {};
+    (function () {
+
+        var before = wsHook.before = function (data, url) {
+            // ...
+            return data;
+        };
+        wsHook.resetHooks = function () {
+            wsHook.before = before;
+        }
+
+        var _WS = WebSocket;
+        WebSocket = function (url, protocols) {
+            var WSObject;
+            this.url = url;
+            this.protocols = protocols;
+            if (!this.protocols) {
+                WSObject = new _WS(url);
+            }
+            else {
+                WSObject = new _WS(url, protocols);
+            }
+
+            var _send = WSObject.send;
+            var _wsobject = this;
+            WSObject.send = function (data) {
+                data = wsHook.before(data, WSObject.url) || data;
+                if (data.indexOf('updateToRead') != -1) {
+                    console.log('receipt is intercepted: ', data)
+                    return;
+                }
+                _send.apply(this, arguments);
+            }
+            return WSObject;
+        }
+    })();
+}
+
+chrome.storage.local.get(null, function (db) {
+    if (document.domain.indexOf('im.dingtalk.com') != -1) {
+        console.log('Injecting DingTalk hooks.');
+        var injectString = [];
+        injectString.push(wshook)
+
+        // Generate the script to inject from the array of functions.
+        var scriptToInject = "";
+
+        injectString.forEach(function (func) {
+            var func = func.toString().trim(); // get the function code
+            func = func.replace(func.split('{', 1), ''); // remove the function(), part from the string.
+            func = func.substr(1); // remove the {
+            func = func.substr(0, func.length - 1); // remove the last } at the end.
+            func = func + "\n"; // create a new line
+            scriptToInject = scriptToInject + func;
+
+        });
+
+        injectScript(scriptToInject);
+
+    }
+});
+
+function injectScript(scriptString) {
+    var actualCode = '(function(){' + scriptString + '})();'
+    var script = document.createElement('script');
+    script.textContent = actualCode;
+    (document.head || document.documentElement).appendChild(script);
+    script.parentNode.removeChild(script);
+}
